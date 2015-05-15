9a101c13fdfa501c50a03e8bce61844a039ca108
JimSeker <seker@uwyo.edu>
2015-05-15 09:01:05 -0600

    updated to AppCompatActivity and API22.
    Also added some comments.

--- blueToothDemo/app/src/main/java/edu/cs4730/btDemo/MainActivity.java
+++ blueToothDemo/app/src/main/java/edu/cs4730/btDemo/MainActivity.java
@@ -1,15 +1,21 @@
 package edu.cs4730.btDemo;
 
-import java.util.Locale;
 import java.util.UUID;
-
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
+/*
+ * most of the work is done in the fragments.  You will need to install this example onto 2 devices
+ * with bluetooth in order to make this example work.
+ *
+ * The help fragment starts up first, to check and see if this example will work.
+ * Server fragment is the for the "server" code version with bluetooth.
+ * Client fragment is will connect to the server code.
+ */
 
-public class MainActivity extends ActionBarActivity implements Help_Fragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements Help_Fragment.OnFragmentInteractionListener {
 
 	public static final UUID MY_UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
 	public static final String NAME = "BluetoothDemo";
