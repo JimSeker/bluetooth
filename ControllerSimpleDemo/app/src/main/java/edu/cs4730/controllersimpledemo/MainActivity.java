12cd224a94919251e795a6578d5933bf25759b33
JimSeker <seker@uwyo.edu>
2016-12-12 20:39:34 -0700

    Added bluetooth controller Demo

---
+++ ControllerSimpleDemo/app/src/main/java/edu/cs4730/controllersimpledemo/MainActivity.java
@@ -0,0 +1,116 @@
+package edu.cs4730.controllersimpledemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.InputDevice;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView name, last, logger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = (TextView) findViewById(R.id.Name);
+        last = (TextView) findViewById(R.id.lastBtn);
+        logger = (TextView) findViewById(R.id.logger);
+
+        getGameControllerIds();
+
+    }
+
+
+    @Override
+    public boolean onGenericMotionEvent(android.view.MotionEvent motionEvent) {
+        float xaxis = motionEvent.getAxisValue(MotionEvent.AXIS_HAT_X);
+        float yaxis = motionEvent.getAxisValue(MotionEvent.AXIS_HAT_Y);
+
+        boolean handled = false;
+        // Check if the AXIS_HAT_X value is -1 or 1, and set the D-pad
+        // LEFT and RIGHT direction accordingly.
+        if (Float.compare(xaxis, -1.0f) == 0) {
+           // Dpad.LEFT;
+            last.setText("Dpad Left");
+            handled = true;
+        } else if (Float.compare(xaxis, 1.0f) == 0) {
+            // Dpad.RIGHT;
+            last.setText("Dpad Right");
+            handled = true;
+        }
+        // Check if the AXIS_HAT_Y value is -1 or 1, and set the D-pad
+        // UP and DOWN direction accordingly.
+        else if (Float.compare(yaxis, -1.0f) == 0) {
+            // Dpad.UP;
+            last.setText("Dpad Up");
+            handled = true;
+        } else if (Float.compare(yaxis, 1.0f) == 0) {
+            // Dpad.DOWN;
+            last.setText("Dpad Down");
+            handled = true;
+        }
+        if (!handled) {
+            logger.append("dPad: X " + xaxis + " Y " + yaxis +"\n");
+        }
+        return handled;
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(android.view.KeyEvent event) {
+        if ((event.getSource() & InputDevice.SOURCE_GAMEPAD)
+                == InputDevice.SOURCE_GAMEPAD) {
+
+            if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                switch (event.getKeyCode()) {
+                    case KeyEvent.KEYCODE_BUTTON_X:
+                        last.setText("X Button");
+                        break;
+                    case KeyEvent.KEYCODE_BUTTON_A:
+                        last.setText("A Button");
+                        break;
+                    case KeyEvent.KEYCODE_BUTTON_Y:
+                        last.setText("Y Button");
+                        break;
+                    case KeyEvent.KEYCODE_BUTTON_B:
+                        last.setText("B Button");
+                        break;
+
+                }
+
+                logger.append("code is " + event.getKeyCode() + "\n");
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    //From Google's page on controller-input
+    public ArrayList getGameControllerIds() {
+        ArrayList gameControllerDeviceIds = new ArrayList();
+        int[] deviceIds = InputDevice.getDeviceIds();
+        for (int deviceId : deviceIds) {
+            InputDevice dev = InputDevice.getDevice(deviceId);
+            int sources = dev.getSources();
+
+            // Verify that the device has gamepad buttons, control sticks, or both.
+            if (((sources & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD)
+                    || ((sources & InputDevice.SOURCE_JOYSTICK)
+                    == InputDevice.SOURCE_JOYSTICK)) {
+                // This device is a game controller. Store its device ID.
+                name.setText(dev.getName() );
+                if (!gameControllerDeviceIds.contains(deviceId)) {
+                    gameControllerDeviceIds.add(deviceId);
+                }
+            }
+        }
+        return gameControllerDeviceIds;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 Bluetooth Example
 ===========
 
-BlueToothDemo is a simple example of how to startup a bluetooth client and server.  You will need to install
+<b>BlueToothDemo</b> is a simple example of how to startup a bluetooth client and server.  You will need to install
 this example on two devices (not the simulators, doesn't work there).  Once the connections are made, it just 
 like any other network code.
+
+
+<b>ControllerSimpleDemo</b> is a simple demo for using a bluetooth controller like a joystick or game controller
+
+<b>controller</b> is a google's example for the Controller.  Nothing was changed, only it's now setup for studio.
 
 These are example code for University of Wyoming, Cosc 4730 Mobile Programming course.
 All examples are for Android.
